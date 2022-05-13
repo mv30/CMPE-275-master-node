ebf157f3b198a603af91402220dcc22058fde0dc
mayankgarg23 <mayank.garg@sjsu.edu>
2022-05-13 13:04:24 -0700

    hash function to find appropriate node

--- proxy_node/src/main/java/service/ReplicationService.java
+++ proxy_node/src/main/java/service/ReplicationService.java
@@ -3,11 +3,46 @@ package service;
 import io.grpc.stub.StreamObserver;
 import org.master.protos.*;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ReplicationService extends ReplicationGrpc.ReplicationImplBase {
+
+    private String convertToHex(final byte[] messageDigest) {
+        BigInteger bigint = new BigInteger(1, messageDigest);
+        String hex = bigint.toString(16);
+        while (hex.length() < 32) {
+            hex = "0".concat(hex);
+        }
+        return hex;
+    }
+
+    public String createHash(String input) throws NoSuchAlgorithmException {
+        String hashString = null;
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] messageDigest = md.digest(input.getBytes());
+        hashString = convertToHex(messageDigest);
+        return hashString;
+    }
+
+    public int findNode(String hashString) {
+        BigInteger decimal = new BigInteger(hashString, 16);
+        int node = decimal.mod(new BigInteger("3")).intValue(); // TODO: dynamic number of nodes to modulo
+        return node;
+    }
+
     @Override
     public void newNodeUpdate(NewNodeUpdateRequest request, StreamObserver<StatusResponse> responseObserver) {
         String node_ip = request.getNewnodeip();
-        System.out.println("Hello " + node_ip);
+        int node = 0;
+        try {
+            String hash_value = createHash(node_ip);
+            node = findNode(hash_value);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        System.out.println(node);
         StatusResponse.Builder response = StatusResponse.newBuilder();
         response.getStatusValue();
 
